fd8bff309683beea7ff85c17c6b55e0a4b125e30
Youssef Alashmawy <36681247+JOoAlashmawy@users.noreply.github.com>
2021-07-06 18:51:14 +0200

    Update Main.java

--- Java/Titanic/src/main/java/Main.java
+++ Java/Titanic/src/main/java/Main.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        
+        //The first task
+        Table data = new ReadFile().readCsvFile();
+        System.out.println(data.summary());
+        // or we can use dataFrame too
+
+        //The second task
         //Read File
         List<Passenger> node = new ReadFile().readJsonFile();
-        System.out.println(node);
+//        System.out.println(node);
 
         //xChart
         Graph.graphPassengerAges(node);
