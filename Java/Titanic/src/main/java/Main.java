d451de0175c1f191262e23b9e218f9b683db5014
Youssef Alashmawy <36681247+JOoAlashmawy@users.noreply.github.com>
2021-07-06 18:53:04 +0200

    Update Main.java

--- Java/Titanic/src/main/java/Main.java
+++ Java/Titanic/src/main/java/Main.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
 import java.util.List;
+import tech.tablesaw.api.Table;
 
 public class Main {
     public static void main(String[] args) throws IOException {
